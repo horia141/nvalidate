d7cb36c104e4e80ef79b0ce0e836182d421eeec5
Horia Coman <hcoman@stackoverflow.com>
2017-03-28 16:27:24 +0300

    Use dotnet nuget push to do the pushing

--- tools/tonuget.sh
+++ tools/tonuget.sh
@@ -3,7 +3,5 @@
 echo 'Here'
 dotnet pack --configuration release src/NValidate.csproj
 echo "Packed $?"
-nuget setApiKey $1 -source https://www.nuget.org
-echo "Set API key $?"
-nuget push src/bin/release/NValidate.*.nupkg -Source https://www.nuget.org/api/v2/package
+dotnet nuget push src/bin/release/NValidate.*.nupkg --api-key $1
 echo "Pushed $?"
