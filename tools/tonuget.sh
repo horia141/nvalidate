006068ea8b29e16db5d04b3a478e8bafb7466a43
Horia Coman <hcoman@stackoverflow.com>
2017-03-28 16:38:42 +0300

    Specify the source for pushing the package

--- tools/tonuget.sh
+++ tools/tonuget.sh
@@ -3,5 +3,6 @@
 echo 'Here'
 dotnet pack --configuration release src/NValidate.csproj
 echo "Packed $?"
-dotnet nuget push src/bin/release/NValidate.*.nupkg --api-key $1
+dotnet nuget push src/bin/release/NValidate.*.nupkg --api-key $1 --source https://www.nuget.org/api/v2/package
 echo "Pushed $?"
+exit $?
