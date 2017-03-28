d98293fb77c3a737403d3f78b495e6511590e7c4
Horia Coman <hcoman@stackoverflow.com>
2017-03-28 16:09:13 +0300

    Try some debugging on the script.

--- tools/tonuget.sh
+++ tools/tonuget.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+echo 'Here'
 dotnet pack --configuration release src/NValidate.csproj
+echo "Packed $?"
 nuget setApiKey $1 -source https://www.nuget.org
+echo "Set API key $?"
 nuget push src/bin/release/NValidate.*.nupkg -Source https://www.nuget.org/api/v2/package
+echo "Pushed $?"
