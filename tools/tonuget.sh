5402817b508e05b615c613dd44cf2e0e4cea1418
Horia Coman <hcoman@stackoverflow.com>
2017-03-28 16:54:26 +0300

    Don't use echos in deploy. The commands are verbose enough

--- tools/tonuget.sh
+++ tools/tonuget.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-echo 'Here'
 dotnet pack --configuration release src/NValidate.csproj
-echo "Packed $?"
 dotnet nuget push src/bin/release/NValidate.*.nupkg --api-key $1 --source https://www.nuget.org/api/v2/package
-echo "Pushed $?"
 exit $?
