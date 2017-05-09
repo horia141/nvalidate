ac863113499207baf9f5f98dc4ada9c80bacd8db
Horia Coman <horia141@gmail.com>
2017-05-09 19:01:02 +0300

    Added the ExceptionsAttribute

---
+++ src/ExceptionsAttribute.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+
+namespace NValidate
+{
+    /// <summary>
+    /// Identifies which of the entities produced by a projector should be ignored as exceptions to the "rule".
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
+    public class ExceptionsAttribute : Attribute
+    {
+        object[] _exceptionsSet;
+
+
+        public ExceptionsAttribute(params object[] exceptionsSet)
+	{
+	    _exceptionsSet = exceptionsSet;
+	}
+	
+
+        public ISet<object> GetExceptionsSet() => new HashSet<object>(_exceptionsSet);
+	
+	
+        public static bool HasAttribute(MethodInfo methodInfo) => methodInfo.GetCustomAttributes(typeof(ExceptionsAttribute), true).Count() > 0;
+    }
+}
