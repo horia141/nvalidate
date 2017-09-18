885e96c30e2ccb24815e4fb751843bd323f6ff55
Horia Coman <horia141@gmail.com>
2017-09-18 17:03:33 +0300

    Add tests for SkipAttribute

---
+++ test/SkipAttributeTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System.Reflection;
+
+namespace NValidate.Tests
+{
+    [TestFixture]
+    public class SkipAttributeTest
+    {
+        [Skip]
+        class ClassWithAttribute { }
+
+        class ClassWithoutAttribute { }
+
+        [Skip]
+        public void FunctionWithAttribute() { }
+
+        public void FunctionWithoutAttribute() { }
+
+        [Test]
+        public void HasAttributeForClass()
+        {
+            Assert.That(SkipAttribute.HasAttribute(typeof(ClassWithAttribute).GetTypeInfo()), Is.True);
+            Assert.That(SkipAttribute.HasAttribute(typeof(ClassWithoutAttribute).GetTypeInfo()), Is.False);
+        }
+
+        [Test]
+        public void HasAttributeForFunction()
+        {
+            Assert.That(SkipAttribute.HasAttribute(typeof(SkipAttributeTest).GetMethod("FunctionWithAttribute")), Is.True);
+            Assert.That(SkipAttribute.HasAttribute(typeof(SkipAttributeTest).GetMethod("FunctionWithoutAttribute")), Is.False);
+        }
+    }
+}
