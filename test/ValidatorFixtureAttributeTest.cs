1e55ce4d05c839922f1b11beb42e7de91b2723c9
Horia Coman <horia141@gmail.com>
2017-09-18 16:50:36 +0300

    Add tests for the ValidatorFixtureAttribute

---
+++ test/ValidatorFixtureAttributeTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Reflection;
+
+namespace NValidate.Tests
+{
+    [TestFixture]
+    public class ValidatorFixtureAttributeTest
+    {
+        [ValidatorFixture("WithAttribute")]
+        class ClassWithAttribute { }
+
+        class ClassWithoutAttribute { }
+
+        [Test]
+        public void Name()
+        {
+            var attribute = new ValidatorFixtureAttribute("The Attribute");
+
+            Assert.That(attribute.Name, Is.EqualTo("The Attribute"));
+        }
+
+        [Test]
+        public void HasAttribute()
+        {
+            Assert.That(ValidatorFixtureAttribute.HasAttribute(typeof(ClassWithAttribute).GetTypeInfo()), Is.True);
+            Assert.That(ValidatorFixtureAttribute.HasAttribute(typeof(ClassWithoutAttribute).GetTypeInfo()), Is.False);
+        }
+    }
+}
