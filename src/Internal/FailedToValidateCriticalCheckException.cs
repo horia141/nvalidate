9ea63c90dd1131e495e583172fa1317563af00c0
Horia Coman <horia141@gmail.com>
2017-09-18 20:32:50 +0300

    Some more comments

--- src/Internal/FailedToValidateCriticalCheckException.cs
+++ src/Internal/FailedToValidateCriticalCheckException.cs
@@ -6,6 +6,11 @@ namespace NValidate.Internal
     /// <summary>
     /// Exception thrown when a critical condition is not satisfied.
     /// </summary>
+    /// <remarks>
+    /// When a runner for an validator instnce checks a constraint defined via a 
+    /// <see cref="CheckRecorder.CriticalThat{T}(T, NUnit.Framework.Constraints.IResolveConstraint)"/> and it fails,
+    /// this exception is thrown, so the execution of the whole instance is stopped.
+    /// </remarks>
     class FailedToValidateCriticalCheckException : Exception
     {
     }
