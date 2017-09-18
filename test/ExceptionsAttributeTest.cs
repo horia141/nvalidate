a0984f89b1bce138e984ab3299e643eb34ae0f39
Horia Coman <horia141@gmail.com>
2017-09-18 16:21:14 +0300

    Add tests for the ExceptionsAttribute

---
+++ test/ExceptionsAttributeTest.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace NValidate.Tests
+{
+    [TestFixture]
+    public class ExceptionsAttributeTest
+    {
+        [Test]
+        public void GetExceptionsSetEmpty()
+        {
+            var attribute = new ExceptionsAttribute();
+
+            Assert.That(attribute.GetExceptionsSet(), Is.Empty);
+        }
+
+        [Test]
+        public void GetExceptionsSetOneElement()
+        {
+            var attribute = new ExceptionsAttribute(10);
+
+            Assert.That(attribute.GetExceptionsSet(), Is.EquivalentTo(new[] { 10 }));
+        }
+
+        [Test]
+        public void GetExceptionsSetSeveralElements()
+        {
+            var attribute = new ExceptionsAttribute(10, 20, 30);
+
+            Assert.That(attribute.GetExceptionsSet(), Is.EquivalentTo(new[] { 30, 20, 10 }));
+        }
+
+        [Exceptions(10, 20, 30)]
+        public void FunctionWithAttribute() { }
+
+        public void FunctionWithoutAttribute() { }
+
+        [Test]
+        public void HasAttribute()
+        {
+            Assert.That(ExceptionsAttribute.HasAttribute(typeof(ExceptionsAttributeTest).GetMethod("FunctionWithAttribute")), Is.True);
+            Assert.That(ExceptionsAttribute.HasAttribute(typeof(ExceptionsAttributeTest).GetMethod("FunctionWithoutAttribute")), Is.False);
+        }
+    }
+}
