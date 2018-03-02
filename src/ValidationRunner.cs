d958c2914090c3e29b7b103358ccfa23976e4b55
Horia Coman <horia141@gmail.com>
2018-03-02 18:34:05 +0200

    Add comments for the ValidationRunner

--- src/ValidationRunner.cs
+++ src/ValidationRunner.cs
@@ -8,18 +8,39 @@ using System.Reflection;
 
 namespace NValidate
 {
+    /// <summary>
+    /// The "entry point" for the whole validation process. You provide an <see cref="Environ"/>
+    /// which provides the various data needed by validators, and an assembly where validators can be
+    /// found and it does the rest. That is, it'll run all the validators in the ways they're meant,
+    /// record the results and build a <see cref="ValidatorRunResult"/> describing the run's status.
+    /// </summary>
+    /// <remarks>
+    /// Currently there is an assumption that this thing will be run once a day. It's not baked in,
+    /// and perhaps it would make better sense to provide a date in the <see cref="Run(Assembly)"/>
+    /// method, but that's the way it is for now.
+    /// </remarks>
     public class ValidationRunner
     {
         DateTime _dateToRun;
         Environ _environ;
 
 
+        /// <summary>
+        /// Build an instance of this for a day and <see cref="Environ"/>.
+        /// </summary>
+        /// <param name="dateToRun">The day this validation run is for</param>
+        /// <param name="environ">A source of data to validate</param>
         public ValidationRunner(DateTime dateToRun, Environ environ)
         {
             _dateToRun = dateToRun;
             _environ = environ;
         }
 
+        /// <summary>
+        /// Perform the validation run, mark of any issues and return issues as well as analytics.
+        /// </summary>
+        /// <param name="currentAssembly">An assembly to pickup fixtures and templates from</param>
+        /// <returns></returns>
         public ValidatorRunResult Run(Assembly currentAssembly)
         {
             var validatorRunResult = new ValidatorRunResult();
